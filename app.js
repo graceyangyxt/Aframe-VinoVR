d48c88b62cdbadc98806f77381509f00932fc036
Sean Weber <sean.weber95@gmail.com>
2021-07-05 13:00:25 -0400

    fixed port setting to work with heroku

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ console.log("server started");
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
+var port = process.env.PORT || 8080;
 app.get('/',function(req, res) {//listens for when the client opens the site.
     res.sendFile(__dirname + '/client/index.html');
 });
 app.use('/client',express.static(__dirname + '/client'));//listens for when the client wants files.
-serv.listen(2000);
+serv.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
